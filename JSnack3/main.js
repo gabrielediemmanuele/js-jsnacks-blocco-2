31b5b71c5978ab72f1706f2d834791cbdc36579e
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-26 14:46:11 +0200

    feat: add js script

---
+++ JSnack3/main.js
@@ -0,0 +1,16 @@
+// array numeri interi
+const numbersList = [3, 6, 12, 21, 95, 28, 35, 83, 13, 2, 11];
+
+// variabile di supporto
+let total = 0;
+
+for (let i = 0; i < numbersList.length; i++) {
+  const currentNumber = numbersList[i];
+
+  //calcolo la posizione
+  if (i % 2 == 0) total += currentNumber;
+  if (!(i % 2)) total += currentNumber;
+}
+
+// console log
+console.log(" la somma è: " + total);
